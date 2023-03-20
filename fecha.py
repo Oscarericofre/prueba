7aa06244ae74aeec42133c120fa06cc957543dfc
Oscar Erices Cofre <oscarportero_1997@hotmail.com>
2023-03-20 12:34:21 -0300

    camios

---
+++ fecha.py
@@ -0,0 +1,11 @@
+
+import datetime
+
+fecha = input("Ingrese la fecha en formato dd/mm/aaaa: ")
+dia, mes, anio = map(int, fecha.split('/'))
+
+fecha_objeto = datetime.datetime(anio, mes, dia)
+dias_semana = ('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo')
+dia_semana = dias_semana[fecha_objeto.weekday()]
+
+print("El día de la semana correspondiente a la fecha ingresada es:", dia_semana)
